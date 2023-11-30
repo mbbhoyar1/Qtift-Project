4cd3acae6774b4863bccdcc86bc9797e9f0e438e
mukul <mukulbhoyar123@gmail.com>
2023-11-30 22:39:16 +0530

    modified:   src/componant/Logo.css     modified:   src/componant/Nevbar.css 	modified:   src/componant/Nevbar.js

--- src/componant/Nevbar.js
+++ src/componant/Nevbar.js
@@ -10,7 +10,9 @@ const NavBar = () => {
   return (
     <nav className="navbar">
       
-      <Logo />
+      {/* <div className="Logodiv"> */}
+        <Logo />
+        {/* </div> */}
       <SearchBar placeholder="Search a album of your choice" />
       <Button text="Give Feedback" className="give-feedback-btn" />
     </nav>
