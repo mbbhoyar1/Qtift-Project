1ba43cd4df42153013be73c2602faa3bc67337ef
mukul <mukulbhoyar123@gmail.com>
2023-11-30 23:56:32 +0530

    modified:   src/componant/Logo.js

--- src/componant/Logo.js
+++ src/componant/Logo.js
@@ -2,9 +2,9 @@ import React from "react";
 
 
 // import logoImage from "../assest/logoImage.png"
-import styles from './Logo.module.css';
+//import styles from './Logo.module.css';
 import LogoNew from "../assest/LogoNew.png"
 
-const Logo = () => <img className={styles.logo} src={LogoNew} alt="logo" width={67} />;
+const Logo = () => <img src={LogoNew} alt="logo" width={67} />;
 
 export default Logo;
