b297ad20b3f43f39e86c6a8825ac2707171a1ef7
mukul <mukulbhoyar123@gmail.com>
2023-11-30 16:08:57 +0530

    modified:   src/componant/Logo.js

--- src/componant/Logo.js
+++ src/componant/Logo.js
@@ -3,6 +3,6 @@ import React from "react";
 
 import logoImage from "../assest/logoImage.png"
 
-const Logo = () => <img className="Logo" src={logoImage} alt="logo" width={67}/>;
+const Logo = () => <img className="Logo" src={logoImage} alt="logo" width={67} height= {34}/>;
 
 export default Logo;
